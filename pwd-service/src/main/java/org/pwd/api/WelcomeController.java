13158b0e4abe86196b488a7e6e0b2ce10ec8bd40
Kris <kadamowi@gmail.com>
2015-07-13 22:28:48 +0200

    test

--- pwd-service/src/main/java/org/pwd/api/WelcomeController.java
+++ pwd-service/src/main/java/org/pwd/api/WelcomeController.java
@@ -11,6 +11,6 @@ class WelcomeController {
 
     @RequestMapping("/")
     public Welcome welcome() {
-        return new Welcome("Welcome to PWD service.");
+        return new Welcome("Welcome to PWD service (KA).");
     }
 }
