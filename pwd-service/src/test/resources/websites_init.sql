c7f846bc8cd63bdb3db20a57b2edc89e12c51279
Bartosz Walacik <bwalacik@gmail.com>
2015-08-20 23:05:10 +0200

    new fields

--- pwd-service/src/test/resources/websites_init.sql
+++ pwd-service/src/test/resources/websites_init.sql
@@ -7,10 +7,12 @@ select * from  web_site;
 -------------------------------------------------------------------------------
 select 'insert into website(id, administrative_unit, url) values ('||id||',"'||administrative_unit||'","'||url||'");' from  website;
 
-insert into website(id, administrative_unit, url) values (1,'Urz¹d Gminy ¯abia Wola','http://www.zabiawola.pl');
-insert into website(id, administrative_unit, url) values (2,'Urz¹d Miejski w Korszach','http://www.korsze.pl');
-insert into website(id, administrative_unit, url) values (3,'Urz¹d Miejski w Sêpopolu','http://bip.warmia.mazury.pl/sepopol_gmina_miejsko_-_wiejska');
+insert into website(id, administrative_unit, url, city, voivodeship, county)
+values (1,'UrzÄ…d Gminy Å»abia Wola','http://www.zabiawola.pl','Å»abia Wola','Mazowieckie','Grodziski');
+insert into website(id, administrative_unit, url)values (2,'UrzÄ…d Miejski w Korszach','http://www.korsze.pl');
+insert into website(id, administrative_unit, url) values (3,'UrzÄ…d Miejski w SÄ™popolu','http://bip.warmia.mazury.pl/sepopol_gmina_miejsko_-_wiejska');
 insert into website(id, administrative_unit, url) values (4,'Starostwo Powiatowe w Bartoszycach','http://wrota.warmia.mazury.pl/powiat_bartoszycki');
-insert into website(id, administrative_unit, url) values (5,'Urz¹d Miasta Bartoszyce','http://www.bartoszyce.pl');
-insert into website(id, administrative_unit, url) values (6,'Urz¹d Marsza³kowski Województwa Mazowieckiego','http://www.mazovia.pl');
-insert into website(id, administrative_unit, url) values (7,'Urz¹d Miasta Sto³ecznego Warszawy','http://www.um.warszawa.pl');
+insert into website(id, administrative_unit, url) values (5,'UrzÄ…d Miasta Bartoszyce','http://www.bartoszyce.pl');
+insert into website(id, administrative_unit, url) values (6,'UrzÄ…d MarszaÅ‚kowski WojewÃ³dztwa Mazowieckiego','http://www.mazovia.pl');
+insert into website(id, administrative_unit, url, city, voivodeship, county)
+values (7,'UrzÄ…d Miasta StoÅ‚ecznego Warszawy','http://www.um.warszawa.pl','Warszawa','Mazowieckie','Warszawski');
