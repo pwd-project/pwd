190e41ad8e2ead1a0fa9253ab262fd1deab67b5b
Bartosz Galek <bartosz.galek@allegro.pl>
2015-09-16 22:45:21 +0200

    retry template added to analyser client

--- pwd-service/src/main/java/org/pwd/interfaces/analysis/AnalysisRestClient.java
+++ pwd-service/src/main/java/org/pwd/interfaces/analysis/AnalysisRestClient.java
@@ -10,10 +10,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.policy.TimeoutRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URL;
+import java.time.Duration;
 
 
 /**
@@ -24,6 +27,7 @@ public class AnalysisRestClient {
     private static final Logger logger = LoggerFactory.getLogger(AnalysisRestClient.class);
 
     private final RestTemplate restTemplate = new RestTemplate();
+    private final RetryTemplate retryTemplate = new RetryTemplate();
     private final String analysisEndpointUrl;
     private final Gson gson = new GsonBuilder().create();
 
@@ -31,10 +35,13 @@ public class AnalysisRestClient {
     public AnalysisRestClient(@Value("${analysis.endpoint}") String analysisEndpointUrl) {
         Preconditions.checkArgument(!analysisEndpointUrl.isEmpty());
         this.analysisEndpointUrl = analysisEndpointUrl;
+        TimeoutRetryPolicy timeoutRetryPolicy = new TimeoutRetryPolicy();
+        timeoutRetryPolicy.setTimeout(Duration.ofSeconds(15).toMillis());
+        retryTemplate.setRetryPolicy(timeoutRetryPolicy);
     }
 
     public WebsiteAuditReport getAnalysis(URL websiteUrl) {
-        String response = restTemplate.getForObject(analysisEndpointUrl + "?url={websiteUrl}", String.class, websiteUrl);
+        String response = retryTemplate.execute(request -> restTemplate.getForObject(analysisEndpointUrl + "?url={websiteUrl}", String.class, websiteUrl));
         logger.info("analysis: \n" + response);
         JsonObject json = gson.fromJson(response, JsonElement.class).getAsJsonObject();
         return new WebsiteAuditReport(json);
