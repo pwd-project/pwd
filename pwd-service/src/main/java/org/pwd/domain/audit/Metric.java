437ee124b7b075c69d62bbad204dd36f9506289c
smikulsk <smikulsk@gmail.com>
2015-09-26 23:19:54 +0200

    Nowe metryki z a11y - WARNING

--- pwd-service/src/main/java/org/pwd/domain/audit/Metric.java
+++ pwd-service/src/main/java/org/pwd/domain/audit/Metric.java
@@ -20,13 +20,22 @@ public enum Metric {
     nonExistentAriaRelatedElement(1, "AX_ARIA_02 Atrybut aria-labelledby"),
     requiredAriaAttributeMissing(1, "AX_ARIA_03 Wymagane atrybuty dla elementów z rolami ARIA"),
     badAriaAttributeValue(1, "AX_ARIA_04 Stan i poprawność właściwości elementów ARIA"),
+    mainRoleOnInappropriateElement(2, "AX_ARIA_05 Atrybut role=main powinien występować tylko na kluczowych elementach"),
+    ariaOwnsDescendant(2, "AX_ARIA_06 Atrybut aria-owns nie powinien być używany, jeżeli własność wynika bezpośrednio z DOM"),
+    multipleAriaOwners(2, "AX_ARIA_07 Identyfikator elementu nie może występować więcej niż jeden raz jako wartość atrybutu aria-owns"),
     requiredOwnedAriaRoleMissing(1, "AX_ARIA_08 Właściwe role elementów podrzędnych"),
     ariaRoleNotScoped(1, "AX_ARIA_09 Zakres elementów ARIA"),
     unsupportedAriaAttribute(1, "AX_ARIA_10 Obsługiwane atrybuty ARIA"),
+    badAriaAttribute(2, "AX_ARIA_11 Istnieją elementy z nieprawidłowymi atrybutami ARIA"),
+    ariaOnReservedElement(2, "AX_ARIA_12 Element nie wspiera ról, stanów i właściwości ARIA"),
+    audioWithoutControls(2, "AX_AUDIO_01 Elementy związane z audio powinny mieć kontrolki"),
     duplicateId(1, "AX_HTML_02 Unikalność identyfikatorów elementów"),
-    multipleLabelableElementsPerLabel(1, "AX_TEXT_03 Etykietowanie elementów");
+    multipleLabelableElementsPerLabel(1, "AX_TEXT_03 Etykietowanie elementów"),
+    linkWithUnclearPurpose(2, "AX_TEXT_04 Cel każdego odnośnika powinien być czytelny z jego opisu"),
+    elementsWithMeaningfulBackgroundImage(2, "AX_IMAGE_01 Obrazki mające znaczenie nie powinny być stosowane jako tło"),
+    focusableElementNotVisibleAndNotAriaHidden(2, "AX_FOCUS_01 Istnieją elementy, które mogą uzyskać fokus, ale są niewidoczne albo zakryte przez inne elementy");
 
-private String namePl;
+    private String namePl;
 private int weight;
 
         Metric(int weight, String namePl) {
