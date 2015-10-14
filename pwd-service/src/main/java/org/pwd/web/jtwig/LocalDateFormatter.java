e10386c4e7ea1cf5a12c13df60e222abde607146
Krzysztof Adamowicz <kadamowi@gmail.com>
2015-10-14 23:17:37 +0200

    Kolejna paczka poprawekgrafika

--- pwd-service/src/main/java/org/pwd/web/jtwig/LocalDateFormatter.java
+++ pwd-service/src/main/java/org/pwd/web/jtwig/LocalDateFormatter.java
@@ -7,8 +7,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateFormatter {
+    @JtwigFunction(name = "localDateTime")
+    public String localDateTime(@Parameter LocalDateTime inputDate) {
+        return inputDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    }
+
     @JtwigFunction(name = "localDate")
     public String localDate(@Parameter LocalDateTime inputDate) {
-        return inputDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        return inputDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 }
