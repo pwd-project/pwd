e6da56207d70f6d2fd02342587b04effaaa5f18a
Krzysztof Adamowicz <kadamowi@users.noreply.github.com>
2015-10-20 12:40:12 +0200

    Update WebsiteAuditRepository.java

--- pwd-service/src/main/java/org/pwd/domain/audit/WebsiteAuditRepository.java
+++ pwd-service/src/main/java/org/pwd/domain/audit/WebsiteAuditRepository.java
@@ -20,8 +20,8 @@ public interface WebsiteAuditRepository extends JpaRepository<WebsiteAudit, Inte
     @Query(nativeQuery = true, value = "SELECT * " +
             " FROM   website_audit wa, website w" +
             " WHERE  w.id = wa.website_fk" +
-            " AND    wa.audit_fk = (SELECT MAX(id) FROM audit where process_status = 'DONE')" +
             " AND    CAST(json_extract_path_text(audit_report,'score') as Integer) > 0 " +
+            " AND    wa.audit_fk = (SELECT MAX(id) FROM audit where process_status = 'DONE')" +
             " ORDER BY to_number(json_extract_path_text(audit_report,'score'),'999') DESC" +
             " LIMIT :maxRecords")
     List<WebsiteAudit> getTop(@Param("maxRecords") Integer maxRecords);
@@ -29,16 +29,16 @@ public interface WebsiteAuditRepository extends JpaRepository<WebsiteAudit, Inte
     @Query(nativeQuery = true, value = "SELECT * " +
             " FROM   website_audit wa, website w" +
             " WHERE  w.id = wa.website_fk" +
-            " AND    wa.audit_fk = (SELECT MAX(id) FROM audit where process_status = 'DONE')" +
             " AND    CAST(json_extract_path_text(audit_report,'score') as Integer) > 0 " +
+            " AND    wa.audit_fk = (SELECT MAX(id) FROM audit where process_status = 'DONE')" +
             " ORDER BY to_number(json_extract_path_text(audit_report,'score'),'999') DESC")
     List<WebsiteAudit> getSorted();
 
     @Query(nativeQuery = true, value = "SELECT * " +
             " FROM   website_audit wa, website w" +
             " WHERE  w.id = wa.website_fk" +
-            " AND    wa.audit_fk = (SELECT MAX(id) FROM audit where process_status = 'DONE')" +
             " AND    CAST(json_extract_path_text(audit_report,'score') as Integer) > 0 " +
+            " AND    wa.audit_fk = (SELECT MAX(id) FROM audit where process_status = 'DONE')" +
             " AND    w.id = :websiteId")
     WebsiteAudit getCurrentScore(@Param("websiteId") int websiteId);
 }
