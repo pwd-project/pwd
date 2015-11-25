5c74aeb37c6d00c15b586979875f180fd2080dd1
Slawomir Mikulski <smikulsk@gmail.com>
2015-11-25 22:45:23 +0100

    Zwrócenie twiga jako response w DownloadController

--- pwd-service/src/main/java/org/pwd/web/download/DownloadController.java
+++ pwd-service/src/main/java/org/pwd/web/download/DownloadController.java
@@ -3,6 +3,9 @@ package org.pwd.web.download;
 import com.lyncode.jtwig.JtwigModelMap;
 import com.lyncode.jtwig.JtwigTemplate;
 import com.lyncode.jtwig.configuration.JtwigConfiguration;
+import com.lyncode.jtwig.exception.CompileException;
+import com.lyncode.jtwig.exception.ParseException;
+import com.lyncode.jtwig.exception.RenderException;
 import com.lyncode.jtwig.resource.ClasspathJtwigResource;
 import org.pwd.domain.contact.HtmlEmailMessage;
 import org.pwd.domain.download.Cms;
@@ -24,10 +27,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -79,17 +85,36 @@ public class DownloadController {
             @PathVariable String cmsName,
             @PathVariable long hashCode,
             Model model) {
-        long delay = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC) - hashCode;
-        if (delay < 0 || delay > 3600) return null;//"error_expired";
-
-        String filePath = "/pub/templates/" + cmsName + "/" + templateName + ".zip";
         try {
+            long delay = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC) - hashCode;
+            if (delay < 0 || delay > 3600) {
+                return returnTwigResponse("templates/error_expired.twig");
+            }
+
+            String filePath = "/pub/templates/" + cmsName + "/" + templateName + ".zip";
             logger.info("Downloading: " + filePath);
             return getResourceFileResponse(filePath,templateName + ".zip");
         }
         catch (IOException ex){
-            //TODO: obsługa brakującego szablonu
             logger.error("Caught IOException: " +  ex.getMessage());
+            return returnTwigResponse("templates/error_404.twig");
+        }
+    }
+
+    private ResponseEntity<InputStreamResource> returnTwigResponse(String twigFilename) {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            JtwigTemplate jtwigTemplate = new JtwigTemplate(
+                    new ClasspathJtwigResource(twigFilename),
+                    new JtwigConfiguration());
+            jtwigTemplate.output(outputStream, new JtwigModelMap());
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.parseMediaType("text/html"))
+                    .body(new InputStreamResource(new ByteArrayInputStream(outputStream.toByteArray())));
+        }
+        catch (Exception ex){
+            logger.error("Cannot render twig template. Caught Exception: " +  ex.getMessage());
             return null;
         }
     }
@@ -103,8 +128,9 @@ public class DownloadController {
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
         headers.add("Expires", "0");
-        headers.add("content-disposition", "attachment; filename=" + fileName);
-
+        if( !fileName.isEmpty()){
+            headers.add("content-disposition", "attachment; filename=" + fileName);
+        }
 
         return ResponseEntity
                 .ok()
