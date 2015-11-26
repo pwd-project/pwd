c5d8e4ec83d664657b8837ac5294c8ecda88cd17
Slawomir Mikulski <smikulsk@gmail.com>
2015-11-27 00:05:33 +0100

    Poprawka w teście integracyjnym

--- pwd-service/src/test/groovy/org/pwd/web/download/DownloadTemplateTest.groovy
+++ pwd-service/src/test/groovy/org/pwd/web/download/DownloadTemplateTest.groovy
@@ -2,12 +2,10 @@ package org.pwd.web.download
 
 import org.pwd.application.IntegrationTest
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.io.ByteArrayResource
-import org.springframework.core.io.InputStreamResource
+import org.springframework.boot.test.TestRestTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.client.RestTemplate
 
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -22,12 +20,12 @@ class DownloadTemplateTest  extends IntegrationTest {
 
     def "should download template for proper hashcode"() {
         given:
-        def restTemplate = new RestTemplate();
+        def testRestTemplate = new TestRestTemplate();
         def timestamp = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
         def hashSend = downloadRequestParamsHasher.getHash('T11','WORDPRESS',timestamp);
 
         when:
-        ResponseEntity response = restTemplate.getForObject('http://localhost:8081/pobierz/{template}/{cms}/{hash}/{timestamp}', ResponseEntity.class, 'T11', 'WORDPRESS', hashSend, timestamp)
+        ResponseEntity response = testRestTemplate.getForEntity('http://localhost:8081/pobierz/{template}/{cms}/{hash}/{timestamp}', ResponseEntity.class, 'T11', 'WORDPRESS', hashSend, timestamp)
 
         then:
         response.statusCode == HttpStatus.OK;
@@ -38,12 +36,12 @@ class DownloadTemplateTest  extends IntegrationTest {
 
     def "should not download template for expired hashcode"() {
         given:
-        def restTemplate = new RestTemplate();
+        def testRestTemplate = new TestRestTemplate();
         def timestamp = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)-4000;
         def hashSend = downloadRequestParamsHasher.getHash('T11','WORDPRESS',timestamp);
 
         when:
-        def response = restTemplate.getForObject('http://localhost:8081/pobierz/{template}/{cms}/{hash}/{timestamp}', String.class, 'T11', 'WORDPRESS', hashSend, timestamp);
+        def response = testRestTemplate.getForObject('http://localhost:8081/pobierz/{template}/{cms}/{hash}/{timestamp}', String.class, 'T11', 'WORDPRESS', hashSend, timestamp);
 
         then:
         response.contains("Przepraszamy link już nie jest aktywny")
@@ -51,12 +49,12 @@ class DownloadTemplateTest  extends IntegrationTest {
 
     def "should not download template for bad template"() {
         given:
-        def restTemplate = new RestTemplate();
+        def testRestTemplate = new TestRestTemplate();
         def timestamp = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
         def hashSend = downloadRequestParamsHasher.getHash('bad_template','WORDPRESS',timestamp);
 
         when:
-        def response = restTemplate.getForObject('http://localhost:8081/pobierz/{template}/{cms}/{hash}/{timestamp}', String.class, 'bad_template', 'WORDPRESS', hashSend, timestamp);
+        def response = testRestTemplate.getForObject('http://localhost:8081/pobierz/{template}/{cms}/{hash}/{timestamp}', String.class, 'bad_template', 'WORDPRESS', hashSend, timestamp);
 
         then:
         response.contains("nic tu nie ma")
@@ -64,12 +62,12 @@ class DownloadTemplateTest  extends IntegrationTest {
 
     def "should not download template for bad cms"() {
         given:
-        def restTemplate = new RestTemplate();
+        def testRestTemplate = new TestRestTemplate();
         def timestamp = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
         def hashSend = downloadRequestParamsHasher.getHash('T11','bad_cms',timestamp);
 
         when:
-        def response = restTemplate.getForObject('http://localhost:8081/pobierz/{template}/{cms}/{hash}/{timestamp}', String.class, 'T11', 'bad_cms', hashSend, timestamp);
+        def response = testRestTemplate.getForObject('http://localhost:8081/pobierz/{template}/{cms}/{hash}/{timestamp}', String.class, 'T11', 'bad_cms', hashSend, timestamp);
 
         then:
         response.contains("nic tu nie ma");
